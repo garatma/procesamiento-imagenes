a86526270d2298e28e6351c46851bfecf8900c8c
Manuel Garat <manuel@mailo.com>
2020-09-25 20:36:12 -0300

    Actividad 1: desomponer y recomponer mal

--- actividades/1/descomponer-canales/hsl/hsl.cpp
+++ actividades/1/descomponer-canales/hsl/hsl.cpp
@@ -33,40 +33,65 @@ void sumar_canal(cv::Mat entrada, cv::Mat salida, int canal) {
 }
 
 int main(int argc, char ** argv) {
+
+    // obtener imagen
     cv::Mat imagen = imread(argv[1], cv::IMREAD_COLOR);
     if(imagen.empty()) {
         std::cout << "No se pudo encontrar la imagen." << std::endl;
         return EXIT_FAILURE;
     }
 
-    cv::cvtColor(imagen, imagen, cv::COLOR_BGR2HSV, 3);
-
     cv::Mat imagenes_canales[imagen.channels()];
     std::string ventanas_canales[imagen.channels()];
+    cv::Mat imagenes_canales_cambiados[imagen.channels()];
+
+    // obtener canales de la imagen y guardarlos como GBR en lugar de RGB
+    for (int i = 0; i < imagen.channels(); i++) {
+        imagenes_canales_cambiados[i] = cv::Mat::zeros(imagen.rows, imagen.cols, CV_8U);
+        extraer_canal(imagen, imagenes_canales_cambiados[i], (i+1)%imagen.channels());
+    }
+
+    cv::cvtColor(imagen, imagen, cv::COLOR_BGR2HSV, 3);
 
     ventanas_canales[0] = "Canal H";
     ventanas_canales[1] = "Canal S";
-    ventanas_canales[2] = "Canal V";
+    ventanas_canales[2] = "Canal L";
 
+    // obtener canales de la imagen HSL y mostrar cada uno por separado
     for (int i = 0; i < imagen.channels(); i++) {
         imagenes_canales[i] = cv::Mat::zeros(imagen.rows, imagen.cols, CV_8U);
         extraer_canal(imagen, imagenes_canales[i], i);
         namedWindow(ventanas_canales[i], cv::WINDOW_NORMAL);
         imshow(ventanas_canales[i], imagenes_canales[i]);
-        cv::moveWindow(ventanas_canales[i], i*OFFSET, 0);
     }
 
     cv::Mat imagen_reconstruida = cv::Mat::zeros(imagen.rows, imagen.cols, CV_8UC3);
+    cv::Mat imagen_reconstruida_cambiados = cv::Mat::zeros(imagen.rows, imagen.cols, CV_8UC3);
 
+    // reconstruir la foto:
+    // - correctamente: de H,S,L a HSL
+    // - incorrectamente: de R,G,B a GBR
     for (int i = 0; i < imagen.channels(); i++) {
         sumar_canal(imagenes_canales[i], imagen_reconstruida, i);
+        sumar_canal(imagenes_canales_cambiados[i], imagen_reconstruida_cambiados, i);
     }
 
+    // graficar la foto en HSL como si fuera RGB
+    namedWindow("Canales HSL como RGB", cv::WINDOW_NORMAL);
+    imshow("Canales HSL como RGB", imagen_reconstruida);
+
+    // convertir de HSL a RGB
     cv::cvtColor(imagen_reconstruida, imagen_reconstruida, cv::COLOR_HSV2BGR, 3);
 
+    // graficar la imagen con los canales mezclados
+    namedWindow("Imagen GBR", cv::WINDOW_NORMAL);
+    imshow("Imagen GBR", imagen_reconstruida_cambiados);
+
+    // graficar la imagen reconstruida correctamente
     namedWindow("Imagen reconstruida", cv::WINDOW_NORMAL);
     imshow("Imagen reconstruida", imagen_reconstruida);
 
+    // esperar cualquier input para salir
     cv::waitKey();
 
     return EXIT_SUCCESS;
