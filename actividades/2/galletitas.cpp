376943c20c290543e428f4915538d3db06146a5b
Manuel Garat <manuel@mailo.com>
2020-10-03 19:05:59 -0300

    Actividad 2: listo
    Estaría bueno agregar un slider para modificar los umbrales (color,
    galletita y fondo).

--- actividades/2/galletitas.cpp
+++ actividades/2/galletitas.cpp
@@ -1,81 +1,104 @@
+#include <algorithm>
+#include <iterator>
 #include <opencv2/core.hpp>
-#include <unistd.h>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 #include <iostream>
-#include <ostream>
 #include <string>
-#define FONDO 100
-#define BLANCO 255
-#define ALTO_GALLETITA 600
-#define LARGO_GALLETITA 600
-#define CANT_GALLETITAS 5
-#define UMBRAL_COLOR 5
-#define UMBRAL_GALLETITA 0.5
-#define ESPACIO_HORIZONTAL 800
-#define ESPACIO_VERTICAL 800
+#include <string.h>
+
+#define   BLANCO               255
+#define   GRIS                 50
+#define   ALTO_GALLETITA       600
+#define   LARGO_GALLETITA      600
+#define   CANT_GALLETITAS      6
+#define   UMBRAL_COLOR         25
+#define   UMBRAL_FONDO		   15
+#define   ESPACIO_HORIZONTAL   800
+#define   ESPACIO_VERTICAL     800
+#define   ARRIBA               0
+#define   ABAJO                1
+#define   CENTRO               2
+#define   IZQUIERDA            3
+#define   DERECHA              4
 
 struct galletita {
 	std::string nombre;
 	uchar colorH;
+	int cantidad;
 };
 
+cv::Mat * copia_imagen, * copia_mapa;
+
+int fondo, umbral_galletita;
+
 // TODO:
-// 	- cuenta de las galletitas al final
-//	- hacerlo independiente de la grilla de 3x3: recorrer toda la imagen y comparar con lo que ya guardé para saltearme esos píxeles
+//	- poner sliders para ajustar el umbral_galletita y el umbral_color
 
 void inicializar_galletitas(struct galletita galletitas[CANT_GALLETITAS])
 {
-	galletitas[0] = { "Claro", 				20  };
-	galletitas[1] = { "Anillo amarillo", 	35  };
-	galletitas[2] = { "Anillo blanco", 		90  };
-	galletitas[3] = { "Anillo rosa", 		170 };
-	galletitas[4] = { "Melba", 				5   };
+	galletitas[0] = { "Marrones", 			15,		0};
+	galletitas[1] = { "Anillo amarillo", 	35,		0};
+	galletitas[2] = { "Anillo blanco", 		90,		0};
+	galletitas[3] = { "Anillo rosa", 		167,	0};
+	galletitas[4] = { "Melba", 				5, 		0};
+	galletitas[5] = { "No identificada",	255,	0};
 }
 
-bool en_umbral(int num1, int num2) { return abs(num1-num2) < UMBRAL_COLOR; }
+bool es(int num1, int num2, int umbral) { return abs(num1-num2) < umbral; }
 
-bool es_galletita(cv::Mat imagen, cv::Mat mapa, int fila, int columna)
+int es_galletita(int fila, int columna, int pos_pixel_anterior, double * suma)
 {
-	int i = fila, j, pixeles_contados = 0, pixeles_correctos = 0;
-	bool es_galletita = false;
 	uchar * p, * pmapa;
-	std::cout << "es_galletita. fila: " << fila << ". columna: " << columna << std::endl;
-	std::cout << "el ancho sería: (" << columna-(LARGO_GALLETITA/2) << "," << columna+(LARGO_GALLETITA/2) << ")" << std::endl;
-	std::cout << "el largo sería: (" << fila << "," << fila+ALTO_GALLETITA << ")" << std::endl;
-	// TODO: guardar el color del pixel acá para comparar luego con el struct galletita
-	while (i < fila+ALTO_GALLETITA) {
-		j = columna-(LARGO_GALLETITA/2);
-		p = imagen.ptr<uchar>(i);
-		pmapa = mapa.ptr<uchar>(i);
-		// std::cout << "correctos: " << pixeles_correctos << std::endl;
-		// std::cout << "contados: " << pixeles_contados << std::endl;
-		// std::cout << "proporción: " << 1.0*pixeles_correctos/pixeles_contados << std::endl << std::endl;
-		while (j < columna+(LARGO_GALLETITA/2)) {
-			if (!en_umbral(p[j], FONDO)) {
-				pmapa[j] = 255;
-				pixeles_correctos++;
-			}
-			pixeles_contados++;
-			j++;
-			// std::cout << "es galletita: " << es_galletita << std::endl;
+	int contador = 0;
+
+	p = (*copia_imagen).ptr<uchar>(fila);
+	pmapa = (*copia_mapa).ptr<uchar>(fila);
+
+	if (!es(p[columna], fondo, UMBRAL_FONDO) && pmapa[columna] == 0) {
+		contador++;
+		pmapa[columna] = BLANCO;
+		(*suma) += int(p[columna]);
+
+		// if (pos_pixel_anterior != ARRIBA && 0 < fila) {
+		// 	pmapa = (*copia_mapa).ptr<uchar>(fila-1);
+		// 	if (pmapa[columna] == 0)
+		// 		contador += es_galletita(fila-1, columna, ABAJO, suma);
+		// }
+
+		if (contador > umbral_galletita) return contador;
+
+		if (pos_pixel_anterior != ABAJO && fila+1 < (*copia_imagen).rows) {
+			pmapa = (*copia_mapa).ptr<uchar>(fila+1);
+			if (pmapa[columna] == 0)
+				contador += es_galletita(fila+1, columna, ARRIBA, suma);
 		}
-		i++;
+
+		if (contador == umbral_galletita) return contador;
+
+		if (pos_pixel_anterior != IZQUIERDA && 0 < columna && pmapa[columna-1] == 0)
+			contador += es_galletita(fila, columna-1, DERECHA, suma);
+
+		if (contador == umbral_galletita) return contador;
+
+		if (pos_pixel_anterior != DERECHA && columna+1 < (*copia_imagen).cols && pmapa[columna+1] == 0)
+			contador += es_galletita(fila, columna+1, IZQUIERDA, suma);
+
+		if (contador == umbral_galletita) return contador;
 	}
-	es_galletita = (1.0*pixeles_correctos/pixeles_contados) > UMBRAL_GALLETITA;
-	std::cout << "proporción: " << (1.0*pixeles_correctos/pixeles_contados) << std::endl;
-	return es_galletita;
+
+	return contador;
 }
 
-int obtener_indice_galletita(struct galletita galletitas[CANT_GALLETITAS], uchar color)
+int obtener_indice_galletita(struct galletita galletitas[CANT_GALLETITAS], int color)
 {
 	bool encontre = false;
 	int i = 0;
-	while (!encontre && i < CANT_GALLETITAS) {
-		encontre = en_umbral(galletitas[i].colorH, color);
+	while (!encontre && i < CANT_GALLETITAS-1) {
+		encontre = es(galletitas[i].colorH, color, UMBRAL_COLOR);
 		i++;
 	}
-	if (!encontre) i = -1;
+	if (encontre) i--;
 	return i;
 }
 
@@ -84,18 +107,18 @@ int main(int argc, char ** argv)
 	struct galletita galletitas[CANT_GALLETITAS];
 	inicializar_galletitas(galletitas);
 
-	// obtener imagen
-	cv::Mat imagen = imread(argv[1], cv::IMREAD_COLOR);
+	cv::Mat imagen;
+
+	if (argc > 1)
+		imagen = imread(argv[1], cv::IMREAD_COLOR);
+
 	if(imagen.empty()) {
 		std::cout << "No se pudo encontrar la imagen." << std::endl;
 		return EXIT_FAILURE;
 	}
 
-	int contador[CANT_GALLETITAS];
-	int galletitas_encontradas = 0;
-
 	for (int i = 0; i < CANT_GALLETITAS; ++i) {
-		contador[i] = 0;
+		galletitas[i].cantidad = 0;
 	}
 
 	cv::cvtColor(imagen, imagen, cv::COLOR_BGR2HSV);
@@ -109,55 +132,62 @@ int main(int argc, char ** argv)
 	cv::Mat cH = canales[0];
 	cv::Mat cV = canales[0];
 
-	bool encontre_primera = false;
+	bool encontre = false;
 	int i = 0, j;
-	uchar * p;
+	uchar * p, *pmapa;
 
-	std::cout << mapa.rows << " " << mapa.cols << std::endl;
+	// establecer color del fondo
+	double suma = 0;
+	p = cH.ptr<uchar>(0);
+	for (int j = 0; j < cH.cols; ++j) suma += p[j];
+	fondo = suma/imagen.cols;
 
-	while (!encontre_primera && i < cH.rows) {
+	int pixeles_galletita;
+
+	umbral_galletita = imagen.cols*imagen.rows/72;
+
+	copia_imagen = &cH;
+	copia_mapa = &mapa;
+	int indice, galletitas_encontradas = 0;
+
+	for (int i = 1; i < cH.rows; ++i) {
 		p = cH.ptr<uchar>(i);
+		pmapa = mapa.ptr<uchar>(i);
 		j = 0;
-		while (!encontre_primera && j < cH.cols) {
-			encontre_primera = !en_umbral(p[j], FONDO);
-			if (encontre_primera) {
-				encontre_primera = es_galletita(cH, mapa, i, j);
-				if (encontre_primera) {
-					std::cout << "encontré en 0,0" << std::endl << std::endl;
+		for (int j = 0; j < cH.cols; ++j) {
+			if (!pmapa[j] && !es(p[j], fondo, UMBRAL_FONDO)) {
+				suma = 0;
+				pixeles_galletita = es_galletita(i, j, CENTRO, &suma);
+				if (pixeles_galletita > umbral_galletita) {
 					galletitas_encontradas++;
-				}
-			}
-			j++;
-		}
-		i++;
-	}
 
-	bool encontre;
-	if (encontre_primera) {
-		int fila = 0, columna = 1;
-		while (fila < 3) {
-			while (columna < 3) {
-				encontre = es_galletita(cH, mapa, i+fila*ESPACIO_VERTICAL, j+columna*ESPACIO_HORIZONTAL);
-				if (encontre) {
-					std::cout << "encontré en " << fila << "," << columna << std::endl << std::endl;
-					galletitas_encontradas++;
+					indice = obtener_indice_galletita(galletitas, (int) suma/pixeles_galletita);
+					galletitas[indice].cantidad++;
 				}
-				columna++;
 			}
-			fila++;
-			columna = 0;
 		}
 	}
 
-	std::cout << "Enhorabuena: has encontrado " << galletitas_encontradas << " galletitas" << std::endl;
+	std::cout << "Se encontraron: " << galletitas_encontradas << " galletitas" << std::endl;
+	std::cout << "Resultado: " << std::endl;
 
-	namedWindow("mapa", cv::WINDOW_NORMAL);
-	imshow("mapa", mapa);
+	for (int i = 0; i < CANT_GALLETITAS; ++i) {
+		std::cout << "\t" << galletitas[i].nombre << ": " << galletitas[i].cantidad << std::endl;
+	}
 
-	namedWindow("cH", cv::WINDOW_NORMAL);
-	imshow("cH", cH);
+	if (argc > 2 && (strcmp(argv[2], "-o") || strcmp(argv[2], "--output"))) {
 
-	cv::waitKey();
+		cv::cvtColor(imagen, imagen, cv::COLOR_HSV2BGR);
+		namedWindow("imagen", cv::WINDOW_NORMAL);
+		imshow("imagen", imagen);
+		cv::moveWindow("imagen", 400, 400);
+
+		namedWindow("mapa", cv::WINDOW_NORMAL);
+		imshow("mapa", mapa);
+		cv::moveWindow("mapa", 800, 400);
+
+		cv::waitKey();
+	}
 
 	return EXIT_SUCCESS;
 }
