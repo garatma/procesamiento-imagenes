9fa51316661a023bdd1945a0dcb9ac2accb35b53
Manuel Garat <manuel@mailo.com>
2020-10-11 14:37:16 -0300

    Actividad 3: algoritmo terminado

--- actividades/3/src/cartoon.cpp
+++ actividades/3/src/cartoon.cpp
@@ -3,7 +3,15 @@
 #include <opencv2/highgui.hpp>
 #include <iostream>
 
-#define CUANTIZADOR 24
+#define CUANTIZADOR 30
+
+int kernel = 1, scale = 0, delta = 0;
+
+void trackbar_callback(int valor, void * parametro)
+{
+	cv::Mat * pimagen = (cv::Mat *) parametro;
+	cv::Mat imagen = *pimagen;
+}
 
 int main(int argc, char ** argv)
 {
@@ -19,19 +27,22 @@ int main(int argc, char ** argv)
 		return EXIT_FAILURE;
 	}
 
+	cv::namedWindow("Imagen entrada", cv::WINDOW_FREERATIO);
+	cv::resizeWindow("Imagen entrada", 500, 500);
+	imshow("Imagen entrada", imagen);
+
 	// (1) - aplicar filtro de la mediana.
 	cv::Mat filtrada;
-	cv::medianBlur(imagen, filtrada, 7);
+	cv::medianBlur(imagen, filtrada, 9);
 
 	// (2) - detectar bordes a la imagen en escala de grises. ¿operaciones para obtenerlo?
 	cv::Mat laplaciano, grises;
-	cv::cvtColor(imagen, grises, cv::COLOR_BGR2GRAY);
-	cv::Laplacian(grises, laplaciano, CV_8U);
-	// cv::dilate(laplaciano, laplaciano, 7);
+	cv::cvtColor(filtrada, grises, cv::COLOR_BGR2GRAY);
+	cv::Laplacian(grises, laplaciano, CV_8U, 1, 1, 1);
 
 	// (3) - reducir la cantidad de colores en (1).
 	cv::Mat bilateral;
-	cv::bilateralFilter(filtrada, bilateral, 9, 50, 50);
+	cv::bilateralFilter(filtrada, bilateral, 15, 50, 50);
 	uchar * fila_bilateral;
 	for (int i = 0; i < bilateral.rows; ++i) {
 		fila_bilateral = bilateral.ptr<uchar>(i);
@@ -42,41 +53,33 @@ int main(int argc, char ** argv)
 
 	// (4) - agregar a (3) los bordes obtenidos en (2).
 	cv::Mat resultado = bilateral.clone();
-
-	// por ahora: si el píxel de (2) no es negro, pintar el de (3) en negro
 	uchar * fila_resultado, * fila_laplaciano;
 	for (int i = 0; i < laplaciano.rows; ++i) {
 		fila_resultado = resultado.ptr<uchar>(i);
 		fila_laplaciano = laplaciano.ptr<uchar>(i);
 		for (int j = 0; j < laplaciano.cols; ++j) {
-			fila_laplaciano[j] = 255-fila_laplaciano[j];
-			if (fila_laplaciano[j] < 255-20) {
+			// fila_laplaciano[j] = 255-fila_laplaciano[j];
+			if (fila_laplaciano[j] > 5) {
 				for (int k = 0; k < resultado.channels(); ++k) {
 					fila_resultado[j*resultado.channels()+k] = 0;
 					fila_laplaciano[j] = 0;
 				}
 			}
-			else fila_laplaciano[j] = 255;
+			// else fila_laplaciano[j] = 255;
 		}
 	}
 
-	// TODO: poner en un único for-for los dos for-for que hay
+	// cv::namedWindow("Filtrada", cv::WINDOW_FREERATIO);
+	// cv::resizeWindow("Filtrada", 500, 500);
+	// imshow("Filtrada", filtrada);
 
-	cv::namedWindow("Imagen entrada", cv::WINDOW_FREERATIO);
-	cv::resizeWindow("Imagen entrada", 500, 500);
-	imshow("Imagen entrada", imagen);
+	// cv::namedWindow("Laplaciano", cv::WINDOW_FREERATIO);
+	// cv::resizeWindow("Laplaciano", 500, 500);
+	// imshow("Laplaciano", laplaciano);
 
-	cv::namedWindow("Filtrada", cv::WINDOW_FREERATIO);
-	cv::resizeWindow("Filtrada", 500, 500);
-	imshow("Filtrada", filtrada);
-
-	cv::namedWindow("Laplaciano", cv::WINDOW_FREERATIO);
-	cv::resizeWindow("Laplaciano", 500, 500);
-	imshow("Laplaciano", laplaciano);
-
-	cv::namedWindow("Bilateral", cv::WINDOW_FREERATIO);
-	cv::resizeWindow("Bilateral", 500, 500);
-	imshow("Bilateral", bilateral);
+	// cv::namedWindow("Bilateral", cv::WINDOW_FREERATIO);
+	// cv::resizeWindow("Bilateral", 500, 500);
+	// imshow("Bilateral", bilateral);
 
 	cv::namedWindow("Resultado", cv::WINDOW_FREERATIO);
 	cv::resizeWindow("Resultado", 500, 500);
